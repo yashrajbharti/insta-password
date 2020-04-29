b4c143bf5af7c109fea85ab1915a2ef98bf0a69f
Yash Raj Bharti <43868318+yashrajbharti@users.noreply.github.com>
2020-04-29 11:50:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function onSubmit() {
+	if (document.getElementById('password').value == '1234') {window.location.href = 'https://www.instagram.com/driftblaze_storms/'; }else{ alert('Access Denied, Please try again');}
+     }
